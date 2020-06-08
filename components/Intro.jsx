82c044d987005f2f7f362e173fc8a754574d358e
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2020-06-07 19:57:16 -0500

    Profile picture done

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Intro = () => (
   <div className="max-w-lg mx-auto p-2 md:p-4 text-center">
     <img
-      className="w-48 h-48 mx-auto rounded-full p-2 md:p-4"
+      className="w-48 h-48 mx-auto rounded-full m-8"
       alt="Profile"
-      src="https://placehold.it/500"
+      src="/ProfessionalImage.jpg"
     />
 
     <div className="p-1 md:p-2">
