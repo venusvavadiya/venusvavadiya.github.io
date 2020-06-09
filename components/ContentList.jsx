7c140725cefb52aed4351f5dcc159fbd7c57325b
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2020-06-09 15:12:09 -0500

    Added

--- components/ContentList.jsx
+++ components/ContentList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ContentList = ({ items }) => (
-  <ul className="pl-8 list-disc">
+  <ul className="pl-4 md:pl-8 list-disc">
     {items.map(i => <li key={i}>{i}</li>)}
   </ul>
 );
