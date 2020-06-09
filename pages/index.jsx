2f390780b93604fff51734039f408626a7110c4c
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2020-06-09 17:05:19 -0500

    Data refactored

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,60 +5,7 @@ import Saperator from "../components/Saperator";
 import ContentBox from "../components/ContentBox";
 import ContentList from "../components/ContentList";
 import ContentHeader from "../components/ContentHeader";
-
-const experiences = [
-  {
-    title: "Software Developer",
-    subtitle: "Leadrevr • Full Time",
-    subsubtitle: "Jan 2019 - May 2020 • 1 Yr 4 Mos",
-    items: [
-      "Designed and built a SASS lead generation tool for used car dealerships.",
-      "Built frontend for inventory management in React.",
-      "Developed backend API using Python and MongoDB.",
-      "Developed automated ad publishing using Python.",
-      "Built various scraping projects to collect automotive data using Python",
-    ],
-  },
-
-  {
-    title: "Technical Systems Analyst Co-op",
-    subtitle: "RBC • Internship",
-    subsubtitle: "Sep 2018 - Dec 2018 • 4 Mos",
-    items: [
-      "Verified the data and documentation of web services across teams.",
-      "Built a proof-of-concept of an internal application for testing in Node.js.",
-      "Created and documented stored procedures in T-SQL for the backup and restore.",
-      "Built solutions through automation that can make developers productive.",
-    ],
-  },
-
-  {
-    title: "Software Developer",
-    subtitle: "Derman Inc. • Part Time",
-    subsubtitle: "Feb 2018 - Aug 2018 • 7 Mos",
-    items: [
-      "Architected the entire web application with my team.",
-      "Built features and implemented business logic.",
-      "Guided and helped the team whenever and wherever required.",
-      "Collaborated with a remote developer.",
-      "Completed the project on time and budget.",
-    ],
-  },
-
-  {
-    title: "Programmer Co-op",
-    subtitle: "Renegade Digital Media Inc. • Internship",
-    subsubtitle: "Sep 2017 - Dec 2017 • 4 Mos",
-    items: [
-      "Built designs provided by the Lead Designer.",
-      "Came up with a scalable and clean solution for functionality in the client's application.",
-      "Learned as much as possible from my mentor.",
-      "Updated and maintained data for current applications.",
-      "Contributed by taking a design initiative on one of the applications.",
-      "Achieved Veeva certification for the company."
-    ],
-  },
-];
+import { experiences } from "../data";
 
 const IndexPage = () => (
   <PageLayout>
