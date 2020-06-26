f9bb6605af3705c4ef0c8f997372db9dbf34d922
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2020-06-26 15:31:14 -0500

    Padding increased in list

--- base/ContentList.jsx
+++ base/ContentList.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 
 const ContentList = ({ items }) => (
-  <ul className="pl-4 list-disc text-sm text-gray-800 leading-relaxed">
-    {items.map(i => <li key={i}>{i}</li>)}
+  <ul className="pl-4 list-disc text-sm text-gray-800">
+    {items.map((i) => (
+      <li
+        key={i}
+        style={{
+          paddingTop: '1px',
+          paddingBottom: '1px',
+        }}
+      >
+        {i}
+      </li>
+    ))}
   </ul>
 );
 
