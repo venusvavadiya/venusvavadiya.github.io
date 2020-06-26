055c39fd97c34666d59b8fbf8fb0d942f158ae28
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2020-06-26 15:26:59 -0500

    line-height increased

--- base/ContentList.jsx
+++ base/ContentList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ContentList = ({ items }) => (
-  <ul className="pl-4 list-disc text-sm text-gray-800">
+  <ul className="pl-4 list-disc text-sm text-gray-800 leading-relaxed">
     {items.map(i => <li key={i}>{i}</li>)}
   </ul>
 );
