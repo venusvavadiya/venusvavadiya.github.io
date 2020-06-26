b0cc3cfe400fb0839d4462714b6c4ca266842f88
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2020-06-26 15:12:20 -0500

    Spacing increased

--- base/ContentBox.jsx
+++ base/ContentBox.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 
 const ContentBox = ({ header, children }) => (
-  <div className="p-1 md:p-2">
-    <div className="p-1 md:p-2">
+  <div className="p-2 md:p-4">
+    <div className="p-2 md:p-4">
       {header}
     </div>
 
     { children && (
-      <div className="p-1 md:p-2">
+      <div className="p-2 md:p-4">
         {children}
       </div>
     )}
